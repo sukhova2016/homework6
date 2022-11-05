956c0ac7339436ee60a514cdbf56082fc7679f60
Polina Drokina <sukhova2016@inbox.ru>
2022-11-05 18:50:35 +0400

    Добавлено решение первой задачи

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите числа через запятую: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)count++;
+}
+Console.WriteLine($"Кол-во введенных чисел > 0: {count}");
